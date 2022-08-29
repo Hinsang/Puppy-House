245b3f4ee10709a811d34d100255d37803c64c63
JunJungyu <14dbwjddl@naver.com>
2022-08-29 21:05:14 +0900

    8월29일

--- puppyhouse/public/JS/index.js
+++ puppyhouse/public/JS/index.js
@@ -1,3 +1,28 @@
-/**
- * 
- */
+
+let slideIndex=0;
+showslides()
+
+
+
+function showslides(){
+	//
+	let slides = document.querySelectorAll('.slide_div')
+	let dots = document.querySelectorAll('.dot')	
+	//console.log(slides)
+	//console.log(dots)
+	for(i=0; i<slides.length; i++){
+		//console.log(i)
+		slides[i].style.display='none'
+	}//forend
+	slideIndex++
+	if(slideIndex>slides.length){slideIndex=1}
+	for(i=0; i<dots.length; i++){
+		dots[i].className = dots[i].className.replace('active')		
+	}
+	slides[slideIndex-1].style.display = 'block'
+	dots[slideIndex-1].className += "active"
+	setTimeout(showslides,2000)	
+}//function end
+
+
+
