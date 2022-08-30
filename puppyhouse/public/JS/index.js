0efc48a909679201b1f5128c8b093cd6938ad73f
JunJungyu <14dbwjddl@naver.com>
2022-08-30 17:01:07 +0900

    백업하라고 하셔서 일단 백업

--- puppyhouse/public/JS/index.js
+++ puppyhouse/public/JS/index.js
@@ -1,28 +1,23 @@
 
-let slideIndex=0;
-showslides()
+let slideIndex = 0;
+showSlides();
 
-
-
-function showslides(){
-	//
-	let slides = document.querySelectorAll('.slide_div')
-	let dots = document.querySelectorAll('.dot')	
-	//console.log(slides)
-	//console.log(dots)
-	for(i=0; i<slides.length; i++){
-		//console.log(i)
-		slides[i].style.display='none'
-	}//forend
-	slideIndex++
-	if(slideIndex>slides.length){slideIndex=1}
-	for(i=0; i<dots.length; i++){
-		dots[i].className = dots[i].className.replace('active')		
-	}
-	slides[slideIndex-1].style.display = 'block'
-	dots[slideIndex-1].className += "active"
-	setTimeout(showslides,2000)	
-}//function end
+function showSlides() {
+  let i;
+  let slides = document.querySelectorAll(".slide_div");
+  let dots = document.querySelectorAll(".dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace("active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
 
 
 
